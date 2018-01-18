29836ef9491c2f3dcae388a1376ae8371d8fc39b
Joel Young <joel@jyng.com>
2018-01-18 18:51:48 -0500

    Added cat maturity levels

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable("Tabby");
+    this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
+    //this.imgAttribution("https://www.flickr.com");
+    this.catLevel = ko.computed(function() {
+        var cnt = this.clickCount();
+        if (cnt <2){
+            return("newborn");
+        } else if (cnt<4){
+            return("adolescent");
+        } else {
+            return("mature");
+        }
+    }, this);
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount()+1);
+    };
+
+}
+
+ko.applyBindings(new ViewModel());
