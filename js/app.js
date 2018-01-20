70ccd891c5d097a5fa8423f801d36b386045a319
Joel Young <joel@jyng.com>
2018-01-20 14:39:45 -0500

    Computer transfer

--- js/app.js
+++ js/app.js
@@ -1,11 +1,48 @@
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable("Tabby");
-    this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
-    // this.imgAttribution("https://www.flickr.com");
-    this.nicknames = ko.observableArray([
-        'Tabtab', 'T-bone', 'Mr. T',
-        'Tabitha Tab Tabby Catty Cat']);
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        srcAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ['Tabtab', 'T-bone', 'Mr. T','Tabitha Tab Tabby Catty Cat']
+    },
+    {
+        clickCount: 0,
+        name: 'Tiger',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        srcAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicknames: ['Tigger']
+    },
+    {
+        clickCount: 0,
+        name: 'Scaredy',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        srcAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+        nicknames: ['Casper']
+    },
+    {
+        clickCount: 0,
+        name: 'Shadow',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+        srcAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ["Shooby"]
+    },
+    {
+        clickCount: 0,
+        name: 'Sleepy',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+        srcAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames: ["Zzzzz"]
+    }
+];
+
+
+var Cat = function (cat) {
+    this.clickCount = ko.observable(cat.clickCount);
+    this.name = ko.observable(cat.name);
+    this.imgSrc = ko.observable(cat.imgSrc);
+    this.srcAttribution = ko.observable(cat.srcAttribution);
+    this.nicknames = ko.observableArray(cat.nicknames);
 
     this.catLevel = ko.computed(function () {
         var cnt = this.clickCount();
@@ -17,17 +54,26 @@ var Cat = function () {
             return ("mature");
         }
     }, this);
-
-}
+};
 
 var ViewModel = function () {
+    var self = this;
 
-    this.currentCat = ko.observable(new Cat());
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(aCat) {
+        self.catList.push(new Cat(aCat));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function () {
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 
+    this.setCurrentCat = function(aCat){
+        self.currentCat = aCat;
+    }
 
 };
 
